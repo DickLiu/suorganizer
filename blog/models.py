3025ca6a4deb6c2212ccfdb286df58b38fdc7a76
Dick Liu <495100061dick@gmail.com>
2017-02-21 09:04:06 +0800

    20170221 adding relational field and field options to control behaivor of fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from organizer.models import Startup, Tag
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField() #如不設定max_length，預設是50
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
+    
--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+    
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(unique=True, help_text='A label for URL config.')
+    description = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('date published') #'date published'是verbose name
+    link = models.URLField(max_length=255)
+    startup = models.ForeignKey(Startup)
+
