76392e89addefb5ff415a817c7a5ef2f831eea5a
Dick Liu <495100061dick@gmail.com>
2017-02-22 15:32:34 +0800

    adding class Meta (ordering/get_latest_by/verbose_name)

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,18 @@ from organizer.models import Startup, Tag
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField() #如不設定max_length，預設是50
+    slug = models.SlugField(max_length=63, help_text='A label for URL config'), unique_for_month='pub_date') #如不設定max_length，預設是50
     text = models.TextField()
-    pub_date = models.DateField()
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
+    pub_date = models.DateField('date published', auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
+    
+    def __str__(self):
+        return "{}:{}".format(self.title, self.pub_date.strftime('%Y-%m-%d')) #將時間用客製化格式顯示的函數strftime()
+        
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
+        
     
--- organizer/models.py
+++ organizer/models.py
@@ -5,19 +5,40 @@ class Tag(models.Model):
     name = models.CharField(max_length=31)
     slug = models.SlugField()
     
+    def __str__(self):
+        return self.name
+        
+    class Meta:
+        ordering = ['name']
+    
 
 class Startup(models.Model):
     name = models.CharField(max_length=31, unique=True)
     slug = models.SlugField(unique=True, help_text='A label for URL config.')
     description = models.TextField()
-    founded_date = models.DateField()
+    founded_date = models.DateField('date founded')
     contact = models.EmailField()
     website = models.URLField(max_length=255)
     tags = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
+        
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
     pub_date = models.DateField('date published') #'date published'是verbose name
     link = models.URLField(max_length=255)
     startup = models.ForeignKey(Startup)
+    
+    def __str__(self):
+        return "{}:{}".format(self.startup, self.title)
+    
+    class Meta:
+        verbose_name = 'news article'
+        ordering = ['-pub_date'] #只有ordering的作用，沒有getting的作用
+        get_latest_by = 'pub_date' #只有getting的作用，沒有ordering的作用
 
