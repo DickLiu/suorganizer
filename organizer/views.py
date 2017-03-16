91bab2f3b0a224c7ba114679555d5c9f15c80e3e
Dick Liu <495100061dick@gmail.com>
2017-03-16 11:40:32 +0800

    adding Django shortcuts get_object_or_404 / render with request argument

--- organizer/views.py
+++ organizer/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-from django.template import Context, loader
+from django.http.response import (HttpResponse, Http404)
+from django.template import RequestContext, loader
 from .models import  Tag
+from django.shortcuts import (get_object_or_404, render)
 # Create your views here.
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list':tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+#    tag_list = Tag.objects.all()
+#    template = loader.get_template('organizer/tag_list.html')
+#    context = RequestContext(request, {'tag_list':tag_list})
+#    output = template.render(context)
+#    return HttpResponse(output)
+#    return render_to_response('organizer/tag_list.html',{'tag_list':Tag.objects.all()})
+    return render(request, 'organizer/tag_list.html', {'tag_list':Tag.objects.all()})        
     
 def tag_detail(request, slug):
-    # slug = ?
-    tag = Tag.objects.get(slug__iexact=slug)
-    template = loader.get_template('organizer/tag_detail.html')
-    context = Context({'tag':tag})
-    return HttpResponse(template.render(context))
+#    # slug = ?
+##    try:
+##        tag = Tag.objects.get(slug__iexact=slug)
+##    except Tag.DoesNotExist:
+##        raise Http404
+    tag = get_object_or_404(Tag, slug__iexact=slug) #replace try...except block
+#    template = loader.get_template('organizer/tag_detail.html')
+#    context = RequestContext(request, {'tag':tag})
+#    return HttpResponse(template.render(context))
+    return render(request, 'organizer/tag_detail.html', {'tag':tag}) #replace render_to_response method
