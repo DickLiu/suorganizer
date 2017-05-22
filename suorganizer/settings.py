1f0cff453bfc58f612be324c6ea69ad72c184cf0
Dick Liu <495100061dick@gmail.com>
2017-05-22 12:13:19 +0800

    ch16 complete

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SERVER_EMAIL = '495100061dick@gmail.com'
