9cb63a2565591f82e9a572de60ee85a088cf9300
Dick Liu <495100061dick@gmail.com>
2017-11-09 18:58:44 +0800

    ch25_complete_after_debugged

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,13 @@
-from django.apps import AppConfig
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 18:36:39 2017
 
+@author: user
+"""
+from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+    
+    def ready(self):
+        import blog.signals
