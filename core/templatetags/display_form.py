5997471829ec3717b7ad43d576fe9c3fddc1d5a7
Dick Liu <495100061dick@gmail.com>
2017-11-26 16:19:56 +0800

    ch27_complete_after_debugged

--- core/templatetags/display_form.py
+++ core/templatetags/display_form.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
 from django.template import (
         Library, TemplateSyntaxError)
+from django.contrib.auth import (get_user, get_user_model)
 
 register = Library()
 
@@ -31,5 +32,45 @@ def form(context, *args, **kwargs):
             'action': action,
             'button': button,
             'form': form,
+            'method': method
+            }
+    
+@register.inclusion_tag(
+        'core/includes/confirm_delete_form.html',
+        takes_context=True)
+def delete_form(context, *args, **kwargs):
+    action = (args[0] if len(args) > 0
+              else kwargs.get('action'))
+    method = (args[1] if len(args) > 1
+              else kwargs.get('method'))
+    form = context.get('form')
+    display_object = kwargs.get(
+            'object', context.get('object'))
+    if display_object is None:
+        request = context.get('request')
+        display_object = get_user(request)
+    if action is None:
+        raise TemplateSyntaxError(
+                "form template tag requires"
+                "at least one argument: action,"
+                "which is a URL.")
+    if display_object is None:
+        raise TemplateSyntaxError(
+                "display_form needs object"
+                "manually specified in the case")
+    if hasattr(display_object, 'name'):
+        obj_title = display_object.name
+    elif hasattr(display_object, 'title'):
+        obj_title = display_object.title.title()
+    else:
+        obj_title = str(display_object)
+    obj_type = kwargs.get(
+            'obj_type', display_object._meta.verbose_name.title())
+    return {
+            'action': action,
+            'form': form,            
+            'obj_type': obj_type,
+            'obj_title': obj_title,
+            'object': display_object,
             'method': method
             }
