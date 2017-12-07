a9101e39b0a55331b99a3d237618a2857d585a4b
Dick Liu <495100061dick@gmail.com>
2017-12-07 12:40:13 +0800

    deploy_13

--- blog/migrations/0007_post_author.py
+++ blog/migrations/0007_post_author.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('blog', '0006_post_permissions'),
+        ('user','0003_user_data'),
     ]
 
     operations = [
