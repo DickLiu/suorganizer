29eea40570e851a10de165049010db5b1dad125f
Dick Liu <495100061dick@gmail.com>
2017-12-24 20:36:35 +0800

    add_amazon_s3_v3

--- suorganizer/settings/base.py
+++ suorganizer/settings/base.py
@@ -167,5 +167,6 @@ FIXTURE_DIRS = (os.path.join(BASE_DIR, 'fixtures'),)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 
